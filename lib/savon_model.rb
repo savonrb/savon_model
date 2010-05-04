85395316e5ec18d36d39dd9055d22f1e2e77bd33
rubiii <me@rubiii.com>
2010-05-04 13:50:54 +0200

    rdoc formatting

--- lib/savon_model.rb
+++ lib/savon_model.rb
@@ -3,7 +3,7 @@ module Savon
   # = Savon::Model
   # ==== Model for SOAP service oriented applications
   #
-  # +Savon::Model+ uses +ActiveModel+ (introduced by Rails 3) to create a pretty basic layer
+  # <tt>Savon::Model</tt> uses +ActiveModel+ (introduced by Rails 3) to create a pretty basic layer
   # for working with service oriented applications that are using SOAP services.
   class Model
   end
