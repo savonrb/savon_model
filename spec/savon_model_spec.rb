85d2042fbfd6ce350ea3ddeecc62a294730ce5f2
rubiii <me@rubiii.com>
2010-05-04 18:37:09 +0200

    the very first basic set of features

--- spec/savon_model_spec.rb
+++ spec/savon_model_spec.rb
@@ -1,5 +1,49 @@
 require "spec_helper"
 
 describe Savon::Model do
-  it "should have specs"
+
+  describe "client (class method)" do
+    before(:all) do
+      @client = User.client :user_service,
+        :endpoint => "http://example.com",
+        :namespace => "http://user_service.example.com"
+    end
+
+    it "should store Savon::Client objects for a given name and options" do
+      @client.should be_a(Savon::Client)
+    end
+
+    it "should set the endpoint specified through the Hash of options" do
+      @client.request.endpoint.should == URI("http://example.com")
+    end
+
+    it "should set the namespace specified through the Hash of options" do
+      Savon::SOAP.namespaces["xmlns:wsdl"].should == "http://user_service.example.com"
+    end
+
+    it "should return the Savon::Client stored under a certain name" do
+      User.client(:user_service).should be_a(Savon::Client)
+    end
+  end
+
+  describe "client (instance method)" do
+    it "should return the Savon::Client stored under a certain name" do
+      User.client :user_service,
+        :endpoint => "http://example.com",
+        :namespace => "http://user_service.example.com"
+      
+      User.new.client(:user_service).should be_a(Savon::Client)
+    end
+  end
+
+  describe "initialize" do
+    it "should accept a Hash of attributes and assign them to instance variables" do
+      user = User.new :id => 1, :name => "Eve", :email => "eve@example.com"
+      
+      user.id.should == 1
+      user.name.should == "Eve"
+      user.email.should == "eve@example.com"
+    end
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,6 @@
 require "spec"
+require "savon_model"
+
+class User < Savon::Model
+  attr_accessor :id, :name, :email
+end
