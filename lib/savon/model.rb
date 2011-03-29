eca62dd4e14f1fd1ee5f14bad1618f5eecf417ac
komba <eugene@russkikh.org.ua>
2011-03-29 11:06:02 +0300

    Added document and http basic

--- lib/savon/model.rb
+++ lib/savon/model.rb
@@ -68,6 +68,14 @@ module Savon
           def endpoint(uri)
             client.wsdl.endpoint = uri
           end
+          
+          def document(uri)
+            client.wsdl.document = uri
+          end
+          
+          def basic_auth(login, password)
+            client.http.auth.basic login, password
+          end
 
           def namespace(uri)
             client.wsdl.namespace = uri
@@ -85,6 +93,14 @@ module Savon
 
           def endpoint(uri)
             self.class.endpoint uri
+          end
+          
+          def document(uri)
+            self.class.document uri
+          end
+          
+          def basic_auth(login, password)
+            self.basic_auth login, password
           end
 
           def namespace(uri)
